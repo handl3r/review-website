967ccd74449420b62d200714d7c6112965ea97c0
Code55x <thaibuixuan34@gmail.com>
2019-07-31 16:58:58 +0700

    update ci

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
 Service.create!(name: 'amusement park')
 Service.create!(name: 'restaurant')
 Service.create!(name: 'resort')
@@ -27,22 +30,22 @@ Faker::Config.locale = 'en-US'
       name: Faker::Name.name,
       address: Faker::Address.street_address << Faker::Address.city << Faker::Address.country,
       website: Faker::Internet.url,
-      phoneNumber: (Faker::PhoneNumber.exchange_code << "-" << Faker::PhoneNumber.exchange_code << "-" << Faker::PhoneNumber.subscriber_number),
+      phoneNumber: (Faker::PhoneNumber.exchange_code << '-' << Faker::PhoneNumber.exchange_code << '-' << Faker::PhoneNumber.subscriber_number),
       email: Faker::Internet.email,
       description: Faker::Lorem.paragraph,
-      password: "111111",
-      password_confirmation: "111111"
-  )
+      password: '111111',
+      password_confirmation: '111111'
+    )
 
 end
-puts "==================="
+puts '==================='
 # Generate 30 places
 
-companys = Array.new
+companys = []
 User.all.each do |user|
-  companys.push(user.id) if user.role == "Company"
+  companys.push(user.id) if user.role == 'Company'
 end
-services = Array.new
+services = []
 Service.all.each do |service|
   services.push(service.id)
 end
@@ -54,16 +57,16 @@ end
              # change after update reviewpaper od user
              numberRatings: 0,
              mediumRating: 0,
-             phoneNumber: Faker::PhoneNumber.exchange_code.to_s << "-" << Faker::PhoneNumber.exchange_code.to_s << "-" << Faker::PhoneNumber.subscriber_number,
+             phoneNumber: Faker::PhoneNumber.exchange_code.to_s << '-' << Faker::PhoneNumber.exchange_code.to_s << '-' << Faker::PhoneNumber.subscriber_number,
              timeOpen: Faker::Time.between(2.days.ago, Date.today, :all),
              timeClose: Faker::Time.between(2.days.ago, Date.today, :all),
              description: Faker::Lorem.paragraph
-    )
+           )
 end
 # Generate Review 
-reviewers = Array.new
+reviewers = []
 User.all.each do |user|
-  reviewers.push(user.id) if user.role == "Reviewer"
+  reviewers.push(user.id) if user.role == 'Reviewer'
 end
 Place.all.each do |place|
   Review.create(
@@ -71,7 +74,7 @@ Place.all.each do |place|
             place_id: place.id,
             rating: Faker::Number.between(1, 5),
             comment: Faker::Lorem.paragraph(10)
-  )
+          )
 end
 # Generate photo 
   # generate avatar
@@ -81,23 +84,22 @@ User.all.each do |user|
            place_id: nil,
            photo_type: 'Avatar',
            path: Faker::File.dir
-  )
+         )
   #generate image place
-  if user.role == 'Company'
-    places_of_company = Array.new
-    Place.all.each do |place|
-      places_of_company.push(place.id) if place.user_id == user.id
-    end
-    places_of_company.each do |p|
-      Photo.create(
-          user_id: user.id,
-          place_id: p,
-          photo_type: 'ImagePlace',
-          path: Faker::File.dir
-          )
-    end
-
+  next unless user.role == 'Company'
+  places_of_company = []
+  Place.all.each do |place|
+    places_of_company.push(place.id) if place.user_id == user.id
   end
+  places_of_company.each do |p|
+    Photo.create(
+        user_id: user.id,
+        place_id: p,
+        photo_type: 'ImagePlace',
+        path: Faker::File.dir
+      )
+  end
+
 end
 
 
