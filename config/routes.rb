105847ef73703c1e33651e7d1f16ba86dbf9418d
Code55x <thaibuixuan34@gmail.com>
2019-08-02 17:34:39 +0700

    fill: layout places, add some filter

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   #get '/user/show/:id' => 'users#show'
   resources :users, :only => [:show]
+  resources :places
   root to: 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
