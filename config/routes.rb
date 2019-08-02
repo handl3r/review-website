f29f5d774eff3786b2b3516a1d2b3fedebd11f5d
Code55x <thaibuixuan34@gmail.com>
2019-08-02 13:05:19 +0700

    fix: header and footer && add : staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
