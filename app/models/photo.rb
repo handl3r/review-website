689b19695f322cf18124f8d4b42eb119391fa659
Code55x <thaibuixuan34@gmail.com>
2019-08-19 23:48:42 +0700

    none

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,4 @@
 class Photo < ApplicationRecord
-   # belongs_to :user
-   # belongs_to :place
   validates :path, uniqueness: true, presence: true
-  enum photo_type: { Avatar: 1, ImagePlace: 2 }
+  enum photo_type: {Avatar: 1, ImagePlace: 2}
 end
